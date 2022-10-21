568caa5ed059b33ddaf084a56f6f6933c244c1e9
nicolassantos2 <116382111+nicolassantos2@users.noreply.github.com>
2022-10-21 16:15:56 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,17 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto! Vcs são NOTA 1000!")
+st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Hamburguers Artesanais do Carlão!")
+st.header("olá")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
